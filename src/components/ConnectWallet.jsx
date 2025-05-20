720922c5ebd606f3e63686cefcc42bd17214efca
Aneesh <aneeshsharmag@gmail.com>
2025-05-20 22:11:13 +0530

    prod fix 6
    all issues fixed

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -15,19 +15,21 @@ const ConnectWallet = () => {
   }, [connected, navigate]);
 
   return (
-    <div className="relative w-full h-screen flex items-center justify-center">
+    <div className="relative w-full h-screen flex items-center justify-center pb-29">
       <div className="animated-bg">
         <div className="blob blob1"></div>
         <div className="blob blob2"></div>
         <div className="blob blob3"></div>
       </div>
       <div className="flex flex-col items-center justify-center z-10 max-w-md w-full">
-        <h1 className="text-3xl font-bold text-[#85fd34] mb-6">Connect Your Wallet</h1>
-        <p className="mb-4 text-gray-100 text-center max-w">
-          You’ll need a Solana-compatible wallet extension to use this app.<br />
-          Supported wallets include <span className="text-white font-semibold">Backpack</span>, <span className="text-white font-semibold">Phantom</span>, <span className="text-white font-semibold">Solflare</span>, <span className="text-white font-semibold">Glow</span>, and more.<br />
-          Please install and configure one of these wallets in your browser.
-        </p>
+        <h1 className="text-4xl font-bold text-[#85fd34] mb-6">Connect Your Wallet</h1>
+        <div className="mb-6 text-gray-200 text-center max-w">
+          <p>You’ll need a Solana-compatible wallet extension to use this app.</p>
+          <p>
+            Supported wallets include <span className="font-semibold text-white">Backpack</span>, <span className="font-semibold text-white">Phantom</span>, <span className="font-semibold text-white">Solflare</span>, <span className="font-semibold text-white">Glow</span>, and more.
+          </p>
+          <p>Please install and configure one of these wallets in your browser.</p>
+        </div>
         <div className="flex gap-4 items-center justify-center mx-auto">
           <WalletMultiButton>
             Connect Wallet
